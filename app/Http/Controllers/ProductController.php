ee93cda7475b902c27d6968bd46d7a22cca6fad3
Gabriel <ollerogabe@gmail.com>
2024-02-21 12:40:45 +0800

    Final Commit: Finalized ProductsController; Initialized web.php and the blade

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+
+        $gadget_array = array(
+
+            "Iphone",
+            "Samsung",
+            "Huawei",
+            "Oppo",
+            "Google Pixel",
+            "Nokia"
+        );
+        return view ('gadgets', ['gadget_array' => $gadget_array]);
+    }
 }
--- resources/views/gadgets.blade.php
+++ resources/views/gadgets.blade.php
@@ -1,3 +1,31 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Midterm Exam - Ollero</title>
+</head>
+<body>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+
+        body{
+            font-family: 'Courier New', Courier, monospace;
+        }
+        ul li{
+            list-style: none;
+            font-size: 2rem;
+        }
+    </style>
+    <h1>Hi! I am Gabriel Ollero. This is my midterm exam and I chose gadgets. Here is the list of my personal preference on best to worst: </h1>
+    <ul>
+    @foreach ($gadget_array as $gadget_list)
+        <li> {{$gadget_list}}</li>
+        @endforeach
+    </ul>
+</body>
+</html>
